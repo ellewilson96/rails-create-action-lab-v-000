551e55709b50d8e594990b1d96b67c0f6041d224
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-03 14:24:34 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,16 +9,16 @@ class StudentsController < ApplicationController
   end
 
   def new
-    @student = Student.new 
+    @student = Student.new
   end
 
   def create
-    @student = Student.new 
+    @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
-    @student.save 
+    @student.save
     redirect_to student_path(@student)
   end
-  
+
 
 end
